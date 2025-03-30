bb95933dca44a448725c7d2466e2b036a8aa527e
Sobolev-Artyom <115024655+Sobolev-Artyom@users.noreply.github.com>
2025-03-30 20:19:12 +0300

    Update main.rs

--- Root/Posting/non_oop/src/main.rs
+++ Root/Posting/non_oop/src/main.rs
@@ -1,13 +1,25 @@
-use blog::Post;
+use matrix::Post;
 
 fn main() {
+    // Add text to post (State = Draft)
     let mut post = Post::new();
 
     post.add_text("I ate a salad for lunch today");
 
+    // Request review (State -> PendingReview)
     let post = post.request_review();
 
-    let post = post.approve(); 
+    // Reject post (State -> Draft)
+    let post = post.reject();
     
+    // Request review again (State -> PendingReview)
+    let post = post.request_review();
+
+    // Aprove post (State = Pending Review)
+    let post = post.approve();
+
+    // Aprove post again (State -> Published)
+    let post = post.approve();
+
     assert_eq!("I ate a salad for lunch today", post.content());
 }
