c464bd5aa3f8cacfe052a4ea70846308bb431f71
Sobolev-Artyom <115024655+Sobolev-Artyom@users.noreply.github.com>
2025-04-27 19:57:09 +0300

    Create main.rs

---
+++ Root/Multithreaded server/mnogopotochnik_rayon/src/main.rs
@@ -0,0 +1,67 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
+use mnogopotochnik::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    // Выполнение нескольких задач
+    let numbers = vec![3, 5, 7, 9];
+    for i in 0..10 {
+        let task_id = i;
+        pool.execute(move || {
+            println!("Task {} is running.", task_id);
+        });
+    }
+    for &number in &numbers {
+        // Пришлем задачу на вычисление факториала в пул потоков
+        pool.execute(move || {
+            let result = factorial(number);
+            println!("Факториал {} равен {}", number, result);
+        });
+    }
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+    println!("Shutting down.");
+    std::thread::sleep(std::time::Duration::from_secs(2));
+}
+
+// Функция для вычисления факториала
+fn factorial(n: u64) -> u64 {
+    (1..=n).product()
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
